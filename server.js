c0323889384197911cc7718938b338c1b5b527c1
Frank <fr.heggeness@gmail.com>
2019-04-20 16:21:38 -1000

    post

--- server.js
+++ server.js
@@ -57,23 +57,62 @@ const server = http.createServer(function(req, res) {
             res.writeHead(500);
             return res.end('{ "success": false }');
           }
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          return res.end('{ "success": true }');
+          fs.readdir(`./public`, (err, files) => {
+            if (err) {
+              throw new Error(err);
+            } else {
+              let numberOfElements = 0;
+              let ignoreFiles = ['server.js', 'elementtest.js', 'css', '.keep'];
+              let fileList = '';
+              files.forEach((file) => {
+                if (!ignoreFiles.includes(file)) {
+                  numberOfElements++;
+                  let fileName = file.slice(0, file.indexOf('.'));
+                  // fileName = fileName[0].toUpperCase + fileName.slice(1);
+                  fileList += `
+                  <li>
+                  <a href="${file}">${fileName}</a>
+                </li>`;
+                }
+              });
+              let indexTemplate = `
+              <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>The Elements</h1>
+  <h2>These are all the known elements.</h2>
+  <h3>These are ${numberOfElements}<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>Document</title>
+      <link rel="stylesheet" href="css/styles.css" />
+    </head>
+    <body></body>
+  </html></h3>
+  <ol>
+    ${fileList}
+  </ol>
+</body>
+</html>`;
+              fs.writeFile(`./public/index.html`, indexTemplate, (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                return res.end('{ "success": true }');
+              });
+            }
+          });
+          // res.writeHead(200, { 'Content-Type': 'application/json' });
+          // return res.end('{ "success": true }');
         });
       }
     });
-
-    // req.on('data', (data) => {
-    //   body = qs.parse(data);
-    //   fs.writeFile(`./public/elements.js`, data, function(err) {
-    //     if (err) {
-    //       return console.log(err);
-    //     }
-
-    //     console.log('The file was saved!');
-    //   });
-    //   return res.end(data);
-    // });
   }
 });
 
